a999592e903c2f2450a669eca496b5c1e878c2b7
Mehran Qadirian <158446870+MehranQadirian@users.noreply.github.com>
2024-11-11 23:15:08 +0330

    Add project files.

---
+++ OrdersTypes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data.Tree BST = new Data.Tree();
+            BST.Insert(30);
+            BST.Insert(35);
+            BST.Insert(57);
+            BST.Insert(15);
+            BST.Insert(63);
+            BST.Insert(49);
+            BST.Insert(89);
+            BST.Insert(77);
+            BST.Insert(67);
+            BST.Insert(98);
+            BST.Insert(91);
+            Console.WriteLine("Inorder Traversal : ");
+            BST.Inorder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Preorder Traversal : ");
+            BST.Preorder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Postorder Traversal : ");
+            BST.Postorder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.ReadLine();
+            Console.WriteLine("Please enter any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
